94050080d388e1f4eefef88d5893487be33ecaae
seymoneg <seymonegug@gmail.com>
2023-01-19 22:24:18 -0500

    add more security features

--- myApp.js
+++ myApp.js
@@ -2,12 +2,25 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet.hidePoweredBy()); // hide how website is powered
-app.use(helmet.frameguard({action: 'deny'})); // prevent clickjacking attempts
-app.use(helmet.xssFilter({})); // prevent malicious scripts being introduced by sanitizing inputs on each request
-app.use(helmet.noSniff({})); // instructs browser to not bypass the content type
-
-
+// hide how website is powered
+app.use(helmet.hidePoweredBy()); 
+// prevent clickjacking attempts
+app.use(helmet.frameguard({action: 'deny'})); 
+// prevent malicious scripts being introduced by sanitizing inputs on each request
+app.use(helmet.xssFilter({})); 
+// instructs browser to not bypass the content type
+app.use(helmet.noSniff({})); 
+//prevent IE users from executing downloads in the trusted site’s context
+app.use(helmet.ieNoOpen({})); 
+// ask user browsers to avoid using insecure HTTP for a specific amount of time
+ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true})) 
+// disable DNS prefetching
+app.use(helmet.dnsPrefetchControl({})); 
+// disable caching so that users download the newest version of software
+app.use(helmet.noCache({})); 
+//set and configure Content Security Policy to prevent the injection of anything unintended into page
+app.use(helmet.contentSecurityPolicy({ directives: {defaultSrc : ["'self'"], scriptSrc : ["'self'", 'trusted-cdn.com']}})); 
 
 
 
